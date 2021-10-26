3fbf76d446e8f50a4811036605508d3fefc09d87
Stephen N <sndegwa.n@outlook.com>
2021-10-26 19:57:02 +0300

    core: application setup
     * install required packages
     * setup test and tests coverage
     * remove files not requred

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import App from "./App";
+
+describe("Test <App/> ", () => {
+  let wrapper: ShallowWrapper | null;
+
+  beforeEach(() => wrapper = shallow(<App />));
+
+  test("Has the text 'Application'", () => {
+    expect(wrapper?.text()).toEqual("Application");
+  })
+});
